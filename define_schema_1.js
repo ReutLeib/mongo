c5585406efe0ea7137e1597c16b12cacc9b5d43b
ReutLeib <leibreut@mail.com>
2018-05-10 20:17:55 +0300

    with define schema

---
+++ define_schema_1.js
@@ -0,0 +1,23 @@
+  // for all module there is one connect
+var mongoose  = require('mongoose'),    
+    // create Schema, for all vars needs to declare a type
+    user      = new mongoose.Schema({
+
+      name: {        
+        type:String,         
+        index:1      
+        },  
+
+      age: Number,
+
+      status: {        
+        type:String,         
+        required:true      
+        },    
+
+      groups: [String]  
+    
+    });
+
+console.log(`required paths: ${user.requiredPaths()}`); 
+console.log(`indexes: ${JSON.stringify(user.indexes())}`);
