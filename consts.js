391268401a0ab90818355c311057ad3884f7e4e1
ReutLeib <leibreut@mail.com>
2018-05-10 21:23:05 +0300

    original

---
+++ consts.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+
+  MLAB_KEY: 'mongodb://db_reut:db_3366@ds119800.mlab.com:19800/db_ringapp2018'
+}
--- mongoose_connect_1.js
+++ mongoose_connect_1.js
@@ -1,13 +1,31 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-app.get('/', function(req, res) {
-    res.send("HELLO");
-});
+// // set the port of our application
+// // process.env.PORT lets the port be set by Heroku
+// var port = process.env.PORT || 8080;
+// app.get('/', function(req, res) {
+//     res.send("HELLO");
+// });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+// app.listen(port, function() {
+//     console.log('Our app is running on http://localhost:' + port);
+// });
+
+var mongoose = require('mongoose'),
+    consts   = require('./consts');
+
+mongoose.connect(consts.MLAB_KEY);
+
+//define the MODEL:
+var User = require('./define_schema_2');
+
+mongoose.connect(consts.MLAB_KEY).then( () =>{
+    console.log(`connected: ${User}`);
+    mongoose.disconnect();
+  },
+  err =>{
+    console.log(`connection error: ${err}`);
+  }
+
+);
