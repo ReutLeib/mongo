584f3c4070bd1947f88892406a0c02b68bebb3ee
ReutLeib <leibreut@mail.com>
2018-05-13 11:20:53 +0300

    try fixed find() at mongoose_find_1

--- mongoose_find_1.js
+++ mongoose_find_1.js
@@ -4,14 +4,13 @@ mongoose.connect(consts.MLAB_KEY);
 //define the MODEL 1:  
 var UserSchema = require('./define_schema_1');
 
- var User = mongoose.model('User',UserSchema);
-
+var User = mongoose.model('User',UserSchema);
 
 mongoose.connect(consts.MLAB_KEY).then( () =>{
     User.find({}, (err,user) => {
       if(err)
         console.log(`query error: ${err}`);
-      console.log(`User: ${User}`);
+      console.log(`User: ${user}`);
       mongoose.disconnect();
     });
     // console.log(`connected: ${User}`);
@@ -19,5 +18,4 @@ mongoose.connect(consts.MLAB_KEY).then( () =>{
   err =>{
     console.log(`connection error: ${err}`);
   }
-
 );
